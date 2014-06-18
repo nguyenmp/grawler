677b46a505c256b80640deac35209e83db329602
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-06-18 16:31:33 -0700

    Fixed keystore loading the old invalid certificate
    UCSB changed their certificate a while back in reaction to the heartblead stuff

---
+++ src/com/nguyenmp/grawler/tests/GrawlerTest.java
@@ -0,0 +1,17 @@
+package com.nguyenmp.grawler.tests;
+
+import java.io.IOException;
+import com.nguyenmp.grawler.*;
+import org.junit.Test;
+
+public class GrawlerTest {
+    @Test
+    public void testLogin() throws HttpClientFactory.SSHException, IOException {
+        GoldSession session = Grawler.login(Credentials.username(), Credentials.password());
+        assert(session != null);
+        assert(session.getCookies() != null);
+        assert(session.getCookies().length > 0);
+        assert(session.getViewState() != null);
+        assert(session.getViewState().length() > 0);
+    }
+}
